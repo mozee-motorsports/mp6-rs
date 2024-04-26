fd252ad293113daccf95b4d08dca60d7f98d6880
Daniel Nimsgern <nimsgernd@msoe.edu>
2024-04-26 11:09:06 -0500

    Comment blocks, pot voltage to percentage for both, averaging percentages, and 10 percent difference checking. These all need testing and SERVO NEEDS RE-TUNING

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 
 mod fmt;
 
-
 use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
@@ -13,20 +12,32 @@ use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
 use embassy_stm32::{
-    adc::{Adc, SampleTime}, gpio::{AnyPin, Level, Output, OutputType, Speed}, peripherals::{TIM3}, time::{hz, khz}, timer::{simple_pwm::{PwmPin, SimplePwm}, Channel}
+    adc::{Adc, SampleTime},
+    gpio::{AnyPin, Level, Output, OutputType, Speed},
+    peripherals::TIM3,
+    time::{hz, khz},
+    timer::{simple_pwm::{PwmPin, SimplePwm}, Channel}
 };
 use embassy_time::{Delay, Duration, Timer};
 use fmt::info;
 
 
-
 static PWM_DUTY_CYCLE: Signal<ThreadModeRawMutex, f32> = Signal::new();
 
-#[embassy_executor::task]
-async fn blink(led_pin: AnyPin) {
-    let mut ld2 = Output::new(led_pin, Level::High /* initial level */, Speed::Low);
+/******************************************************************************
+ * Async Tasks
+ *****************************************************************************/
 
-    loop {
+#[embassy_executor::task]
+async fn blink(led_pin: AnyPin)
+{
+
+    let mut ld2 = Output::new(led_pin,
+                                                        Level::High /* initial level */,
+                                                        Speed::Low);
+
+    loop
+    {
         // let delay_amt = BLINK_MS.load(Ordering::Relaxed);
         let delay_amt = 500; // ms
         // Timer::after returns a future, await yields execution to the executor
@@ -36,26 +47,33 @@ async fn blink(led_pin: AnyPin) {
 }
 
 #[embassy_executor::task]
-async fn pwm_out(mut pwm: SimplePwm<'static, TIM3>) {
+async fn pwm_out(mut pwm: SimplePwm<'static, TIM3>)
+{
     pwm.set_frequency(hz(50));
-    let max = pwm.get_max_duty();
+    // let max = pwm.get_max_duty();
     pwm.enable(Channel::Ch1);
     pwm.enable(Channel::Ch1);
 
     let min_pos: f32 = 100.0;
     let max_pos: f32 = 6000.0;
-    loop {
+
+    loop
+    {
         // waits for a signal on the PWM_DUTY_CYCLE mutex
-        let throttle = (PWM_DUTY_CYCLE.wait().await as f32)/3.3; // 0 to 1
+        let throttle = PWM_DUTY_CYCLE.wait().await as f32; // 0 to 1
         let duty_cycle = (min_pos + (throttle * max_pos) as f32) as u16;
         info!("setting duty cycle to {}", duty_cycle);
         pwm.set_duty(Channel::Ch1, duty_cycle);
     }
 }
 
+/******************************************************************************
+ * Main
+ *****************************************************************************/
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(spawner: Spawner)
+{
 
     /* RCC configuration */
     let mut config = embassy_stm32::Config::default();
@@ -71,7 +89,7 @@ async fn main(spawner: Spawner) {
             divq: Some(PllDiv::DIV8), // SPI1 cksel defaults to pll1_q
             divr: None,
         });
-        config.rcc.pll2 = Some(Pll {
+        config.rcc.pll2 = Some(Pll{
             source: PllSource::HSI,
             prediv: PllPreDiv::DIV4,
             mul: PllMul::MUL50,
@@ -92,53 +110,65 @@ async fn main(spawner: Spawner) {
     /* Peripheral Configuration */
     let mut p = embassy_stm32::init(config);
 
-
     let ch1 = PwmPin::new_ch1(p.PA6, OutputType::PushPull);
-    let mut pwm = SimplePwm::new(p.TIM3, Some(ch1), None, None, None, khz(10), Default::default());
-
+    let pwm = SimplePwm::new(p.TIM3, Some(ch1), None, None, None, khz(10), Default::default());
 
     let mut adc1 = Adc::new(p.ADC1, &mut Delay);
     adc1.set_sample_time(SampleTime::Cycles32_5);
-    let mut adc1_vrefint_channel = adc1.enable_vrefint();
+    // let mut adc1_vrefint_channel = adc1.enable_vrefint();
 
     /* Tasks */
     spawner.spawn(pwm_out(pwm)).unwrap();
 
     /* Main Even Loop: Reading throttle position */
-    loop {
+    loop
+    {
 
         /* Read potentiometers */
         let pot1 = adc1.read(&mut p.PF11) as f32;
         let pot1_v = (pot1/65535.0)*3.3;   // ADC1 is 16-bit
-        info!("pot1: {}V", pot1_v);
+        let pot1_per = pot1_v/3.3;
+        info!("pot1: {} or {}V or {}%\n", pot1, pot1_v, pot1_per);
 
-        // let pot2 = adc2.read(&mut p.PA0) as f32;
-        // let pot2_v = (pot2/65535.0)*3.3;  // ADC2 is 16-bit
-        // let avg = (pot1_v+pot2_v)/2.0;
+        let pot2 = adc1.read(&mut p.PA0) as f32;
+        let pot2_v = (pot2/65535.0)*3.3;  // ADC2 is 16-bit
+        let pot2_per = (3.3-pot2_v)/3.3;
+        info!("pot2: {} or {}V or {}%\n", pot2, pot2_v, pot2_per);
 
-        // if percent_difference(pot1, pot2) > 0.10 {
-        // /* this is an invalid state: report error and close throttle? */
+        let avg = (pot1_per+pot2_per)/2.0;
 
-        // } else {
-        //      /* valid state, set throttle? */
-        //  }
+        if percent_difference(pot1_per, pot2_per) > 0.10
+        {
+        /* this is an invalid state: report error and close throttle? */
 
-        //info!("\npot1: {} V\npot2: {}V\navg: {}V", pot1_v, pot2_v, avg);
+        } 
+        else 
+        {
+             /* valid state, set throttle? */
+        }
 
-        //PWM_DUTY_CYCLE.signal(avg);
+        info!("\npot1: {}%\npot2: {}%\navg: {}%", pot1_per, pot2_per, avg);
+
+        PWM_DUTY_CYCLE.signal(avg);
 
         /* signal the pwm task */
-        PWM_DUTY_CYCLE.signal(pot1_v);
+        // PWM_DUTY_CYCLE.signal(pot1_v);
         Timer::after(Duration::from_millis(100)).await;
     }
 }
 
-pub fn percent_difference(x1: f32, x2: f32) -> f32 {
+/******************************************************************************
+ * Function Definitions
+ *****************************************************************************/
+
+pub fn percent_difference(x1: f32, x2: f32) -> f32
+{
      abs_diff(x1, x2) / (x1 + x2) / 2.0
 }
 
-pub fn abs_diff(x1: f32, x2: f32) -> f32 {
-    if(x1 > x2) {
+pub fn abs_diff(x1: f32, x2: f32) -> f32
+{
+    if x1 > x2 {
         x1 - x2
     } else {
         x2 - x1
